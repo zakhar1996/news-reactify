3fc48ec5de6c754c82fa39e6abe45f50edca0c0a
Zakhar Ruban <zakharruban@gmail.com>
2025-02-23 20:51:47 +0200

    add search input and debounce hook

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,13 +3,33 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL;
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
-export const getNews = async (page_number = 1, page_size = 10) => {
+export const getNews = async ({
+  page_number = 1,
+  page_size = 10,
+  category,
+  keywords,
+}) => {
   try {
     const response = await axios.get(BASE_URL, {
       params: {
         apiKey: API_KEY,
         page_number,
         page_size,
+        category,
+        keywords,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const response = await axios.get("https://api.currentsapi.services/v1/available/categories", {
+      params: {
+        apiKey: API_KEY,
       },
     });
     return response.data;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,23 +1,36 @@
 // import { useEffect } from "react";
 import NewsBanner from "../../components/NewsBanner/NewsBanner";
 import styles from "./styles.module.css";
-import { getNews } from "../../api/apiNews";
+import { getCategories, getNews } from "../../api/apiNews";
 import { useState, useEffect } from "react";
 import NewsList from "../../components/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
 import Pagination from "../../components/Pagination/Pagination";
+import Categories from "../../components/Categories/Categories";
+import Search from "../../components/Search/Search";
+import { useDebounce } from "../../helpers/hooks/useDebounce";
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
+  const [categories, setCategories] = useState([]);
+  const [keywords, setKeywords] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("All");
   const totalPages = 10;
   const pageSize = 10;
+
+  const debouncedKeywords = useDebounce(keywords, 1500);
 
   const fetchNews = async (currentPage) => {
     try {
       setIsLoading(true);
-      const response = await getNews(currentPage, pageSize);
+      const response = await getNews({
+        page_number: currentPage,
+        page_size: pageSize,
+        category: selectedCategory === "All" ? null : selectedCategory,
+        keywords: debouncedKeywords,
+      });
       setNews(response.news);
       setIsLoading(false);
     } catch (error) {
@@ -25,9 +38,22 @@ const Main = () => {
     }
   };
 
+  const fetchCategories = async () => {
+    try {
+      const response = await getCategories();
+      setCategories(["All", ...response.categories]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   useEffect(() => {
     fetchNews(currentPage);
-  }, [currentPage]);
+  }, [currentPage, selectedCategory, debouncedKeywords]);
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -47,12 +73,18 @@ const Main = () => {
 
   return (
     <main className={styles.main}>
+      <Categories
+        categories={categories}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <Search keywords={keywords} setKeywords={setKeywords} />
       {news.length > 0 && !isLoading ? (
         <NewsBanner item={news[0]} />
       ) : (
-        <Skeleton type="banner" count={1} />       
+        <Skeleton type="banner" count={1} />
       )}
-           <Pagination
+      <Pagination
         handlePreviousPage={handlePreviousPage}
         handleNextPage={handleNextPage}
         handlePageClick={handlePageClick}
@@ -64,13 +96,13 @@ const Main = () => {
       ) : (
         <Skeleton type="item" count={10} />
       )}
-            <Pagination
+      <Pagination
         handlePreviousPage={handlePreviousPage}
         handleNextPage={handleNextPage}
         handlePageClick={handlePageClick}
         totalPages={totalPages}
         currentPage={currentPage}
-      /> 
+      />
     </main>
   );
 };
